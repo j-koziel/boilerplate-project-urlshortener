61479214c882ad282252f729c2f4fe4e7a8ef2b7
j-koziel <jonathankoziel1@gmail.com>
2024-08-12 09:46:38 +0100

    fix: handle invalid urls

--- index.js
+++ index.js
@@ -33,29 +33,35 @@ app.get("/api/shorturl/:urlNum", (req, res, next) => {
   res.redirect(urls[urlNum - 1]);
 });
 
-app.post("/api/shorturl", (req, res, next) => {
-  const receivedUrl = req.body.url;
-  const urlObj = new URL(receivedUrl);
+app.post("/api/shorturl", async (req, res, next) => {
+  try {
+    const receivedUrl = req.body.url;
+    const urlObj = new URL(receivedUrl);
 
-  dns.lookup(urlObj.hostname, {}, (err, address, family) => {
-    if (err) {
-      res.status(400);
-      res.json({ error: "invalid url" });
+    dns.lookup(urlObj.hostname, {}, (err, address, family) => {
+      if (err) {
+        res.status(400);
+        res.json({ error: "invalid url" });
+      }
+    });
+
+    urls.forEach((url) => {
+      if (url === receivedUrl) {
+        res.status(400).json({ error: "that url already exists" });
+      }
+    });
+
+    urls.push(receivedUrl);
+
+    res.status(200).json({
+      original_url: receivedUrl,
+      short_url: urls.indexOf(receivedUrl) + 1,
+    });
+  } catch (err) {
+    if (err instanceof TypeError) {
+      res.status(400).json({ error: "invalid url" });
     }
-  });
-
-  urls.forEach((url) => {
-    if (url === receivedUrl) {
-      res.status(400).json({ error: "that url already exists" });
-    }
-  });
-
-  urls.push(receivedUrl);
-
-  res.status(200).json({
-    original_url: receivedUrl,
-    short_url: urls.indexOf(receivedUrl) + 1,
-  });
+  }
 });
 
 app.listen(port, () => {
